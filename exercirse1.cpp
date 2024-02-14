8c175b84eb1b13fc4e4b1e2654c98306b55dead2
Gerald-N <144170512+Gerald-N@users.noreply.github.com>
2024-02-14 11:41:55 +0300

    exercirse1.cpp

--- exercirse1.cpp
+++ exercirse1.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main() {
     float start, end, increment;
 
-    // Accepting input from the user using scanf
     cout << "Enter starting value (in Fahrenheit): ";
     scanf("%f", &start);
     cout << "Enter ending value (in Fahrenheit): ";
@@ -14,14 +13,14 @@ int main() {
     cout << "Enter increment value (in Fahrenheit): ";
     scanf("%f", &increment);
 
-    // Header for the table
+    
     cout << "Fahrenheit  Celsius\n";
 
-    // Loop to print the conversion table
+   
     for(float f = start; f <= end; f += increment) {
         float c = (f - 32.0) * 5.0 / 9.0;
-        cout << fixed; // Setting floating-point precision
-        cout.precision(2); // Setting precision to 2 decimal places
+        cout << fixed;
+        cout.precision(2); 
         cout << f << "         " << c << endl;
     }
 
