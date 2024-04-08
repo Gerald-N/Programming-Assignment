91a3df2e7ce4cac17c1ebe81f86842e0996b09c7
Gerald <144170512+Gerald-N@users.noreply.github.com>
2024-04-08 15:49:19 +0300

    Update EXERCISE 1.c

--- EXERCISE 1.c
+++ EXERCISE 1.c
@@ -1,8 +1,8 @@
 int main()
 {
-    printf("MY NAME IS AYUKA MARITA\n");
+    printf("MY NAME IS GERALD\n");
     printf("MY COURSE IS CIVIL ENGINEERING\n");
-    printf("MY FAVOURITE TEAM IS ARSENAL\n");
-    printf("MY FAVOURITE MEAL IS PILAU\n");
+    printf("MY FAVOURITE TEAM IS WOLVES\n");
+    printf("MY FAVOURITE MEAL IS UGALI BEEF\n");
     return 0;
 }
